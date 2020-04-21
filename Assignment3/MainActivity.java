d15187c73603651330577fc68dc0f96453835135
jason6745 <52573386+jason6745@users.noreply.github.com>
2020-04-21 17:32:59 -0500

    Add files via upload

---
+++ Assignment3/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.assignment3;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Database myDb;
+    TextView editNote;
+    EditText editPreviousNotes;
+    Button btnAddNote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new Database(this);
+
+        editNote = (TextView) findViewById(R.id.editNote);
+        btnAddNote = (Button)findViewById(R.id.btnSave);
+        editPreviousNotes = (EditText)findViewById(R.id.txtPreviousNotes);
+        AddNote();
+    }
+
+    public void AddNote(){
+        btnAddNote.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean result = myDb.createNote(editNote.getText().toString());
+                        if (result)
+                            Toast.makeText(MainActivity.this, "Note Created", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Note Not Created", Toast.LENGTH_LONG).show();
+                        getNotes();
+                        ClearText();
+                    }
+                }
+        );
+    }
+
+    public void getNotes(){
+        Cursor result = myDb.getAllData();
+        StringBuffer buffer = new StringBuffer();
+        while(result.moveToNext()){
+            buffer.append("Note #" + result.getString(0) + ": ");
+            buffer.append(result.getString(1) + "\n\n");
+        }
+
+        MainActivity.this.editPreviousNotes.setText(buffer);
+    }
+
+    public void ClearText(){
+        MainActivity.this.editNote.setText("");
+    }
+}
